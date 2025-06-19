65432aa5d43c348652b486cfe993dc45c56024d5
dea <hopegekdea@gmail.com>
2025-06-19 11:45:16 +0800

    UI DAN TRANSISI ANTAR FRAGMENT BISA (BISA DIJALANKAN)

--- app/src/main/java/com/example/tugasakhirprogmob/fragment/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/example/tugasakhirprogmob/fragment/loginRegister/LoginFragment.kt
@@ -1,19 +1,22 @@
 package com.example.tugasakhirprogmob.fragment.loginRegister
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.tugasakhirprogmob.R
 import com.example.tugasakhirprogmob.activities.LoginRegisterActivity
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val signUpText = view.findViewById<TextView>(R.id.tvSignUp) // ganti dengan ID TextView kamu
+        // Tambahkan log untuk cek apakah fragment berhasil dimuat
+        Log.d("LoginFragment", "Fragment berhasil dimuat")
+
+        val signUpText = view.findViewById<TextView>(R.id.tvSignUp)
         signUpText.setOnClickListener {
             (activity as? LoginRegisterActivity)?.replaceFragment(RegisterFragment())
         }

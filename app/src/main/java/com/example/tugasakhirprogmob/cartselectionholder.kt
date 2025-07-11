eb40bb925d28f1a3ad7e07c5dac4d589c096a282
AyIn04 <ptayuindah04@gmail.com>
2025-07-11 21:25:51 +0800

    tambahan yg kurang utk checkbox

---
+++ app/src/main/java/com/example/tugasakhirprogmob/cartselectionholder.kt
@@ -0,0 +1,7 @@
+package com.example.tugasakhirprogmob.viewmodel
+
+import androidx.compose.runtime.mutableStateListOf
+
+object CartSelectionHolder {
+    val selectedItems = mutableStateListOf<CartItem>()
+}
